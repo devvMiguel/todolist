398c1fdcb6f53b4ba51dc22abdb0428ca8a1ea99
devvMiguel <https://github.com/devvMiguel/todolist>
2023-10-15 15:11:13 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/miguelgalvao/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguelgalvao.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
